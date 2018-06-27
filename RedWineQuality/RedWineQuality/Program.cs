bc0d17c0f54f8499b36ad6f94eff1a424d79a55e
Marcin Wilczyński <wilczynski.pgs@gmail.com>
2018-06-27 23:07:53 +0200

    Create VS solution

---
+++ RedWineQuality/RedWineQuality/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedWineQuality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var stream = new StreamReader(".\\"))
+            using (var helper = new CsvHelper.CsvReader(stream))
+            {
+
+            }
+        }
+    }
+}
